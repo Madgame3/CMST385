f3dc74b25eccaf373f12e9341c7137b480c220c5
Madgame3 <harrington.st@gmail.com>
2016-10-27 21:12:45 +0900

    CMST 385

---
+++ audio/sound.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+//change the /audio/sound1.mp3 to your mp3 location
+
+function sound1() {
+    var audio = new Audio('/audio/sound1.mp3');
+    audio.play();
+}
+
+function sound2() {
+    var audio = new Audio('/audio/sound1.mp3');
+    audio.play();
+}
+function sound3() {
+    var audio = new Audio('/audio/sound1.mp3');
+    audio.play();
+}
