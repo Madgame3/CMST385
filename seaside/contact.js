5fa24b3949c6f4845c152d1ce0e48ec7580e1908
Madgame3 <harrington.st@gmail.com>
2016-10-28 14:41:07 +0900

    case sense

---
+++ seaside/contact.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+function PhoneCall() {
+alert("Please Call: 098-936-2556");
+}
+function EmailUs(){
+    alert("Email us at: Seaside-terrace@yahoo.com");
+}
+function LillyOn() {
+    var hidden = document.getElementById("hidden");
+    hidden.style.display = "inline";
+}
+function LillyOff() {
+    var hidden = document.getElementById("hidden");
+    hidden.style.display = "none";
+}
+function HiLilly() {
+    var lilly = document.getElementById("lilly");
+    lilly = hidden.style.display = "inline";
+}
